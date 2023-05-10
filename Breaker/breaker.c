0b549e1d33fc71ed0d396f3e92e615e14fc5b9a0
Exploitables <crystalwareprojects@gmail.com>
2023-05-10 19:27:07 -0400

    Update breaker.c

--- Breaker/breaker.c
+++ Breaker/breaker.c
@@ -9,9 +9,10 @@ int main(int argc, char** argv)
 	unsigned long long max_length = 0ULL;
 	unsigned long long connection_length = 0ULL;
 	unsigned long long context = 0ULL;
-	unsigned char verbose = 0;
 
 	unsigned long long total_requests = 0ULL;
+	unsigned long long succeeded_requests = 0ULL;
+	unsigned long long failed_requests = 0ULL;
 	char* input = (char*)0;
 	unsigned long size = 0UL;
 	unsigned long bytes_returned = 0UL;
@@ -23,13 +24,12 @@ int main(int argc, char** argv)
 
 	printf("%s", PREAMBLE);
 
-	if (!argv[1] || !argv[2] || !argv[3] || !argv[4] || !argv[5])
+	if (!argv[1] || !argv[2] || !argv[3] || !argv[4])
 	{
-		printf("[*] Usage: Breaker.exe <Communication Port> <Connection Context> <Connection Context Length> <Maximum Input Length> <Verbose>\n");
+		printf("[*] Usage: Breaker.exe <Communication Port> <Connection Context> <Connection Context Length> <Maximum Input Length>\n");
 		return 1;
 	}
 
-	verbose = atoi(argv[5]);
 	max_length = atoi(argv[4]);
 	connection_length = atoi(argv[3]);
 	context = _strtoui64(argv[2], 0, 10);
@@ -73,16 +73,13 @@ int main(int argc, char** argv)
 		}
 
 		h_request_result = FilterSendMessage(h_port, input, size, 0, 0, &bytes_returned);
-		if (verbose)
+		if (h_request_result)
 		{
-			if (h_request_result)
-			{
-				printf("[-] Request failed.\n");
-			}
-			else
-			{
-				printf("[+] Request succeeded.\n");
-			}
+			failed_requests++;
+		}
+		else
+		{
+			succeeded_requests++;
 		}
 
 		free(input);
@@ -90,7 +87,7 @@ int main(int argc, char** argv)
 		total_requests++;
 		if (!(total_requests % 100000))
 		{
-			printf("[*] Total requests sent: %llu (0x%p)\n", total_requests, (void*)total_requests);
+			printf("[*] Total requests sent: %llu\n[*] Total succeeded requests: %llu\n[*] Total failed requests: %llu\n", total_requests, succeeded_requests, failed_requests);
 		}
 	}
 
